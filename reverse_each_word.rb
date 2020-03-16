2a9c7664db64b19744f22aa3f132a1cf958bc1dd
Kathryn Clark <kathrynlael@gmail.com>
2020-03-16 17:50:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require "pry"
 def reverse_each_word(string)
+  return_array = []
   new_array = string.split (/ /)
   new_array.each do |string| 
-  
-  string = "#{b.reverse}"
+  return_array << "#{string.reverse}"
   end 
   return string
 end
